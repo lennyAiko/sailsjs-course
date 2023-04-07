801e187f09d0d8b4db3567c68160bac9dfc57cdf
Lennox Charles <lennyaiko17@gmail.com>
2023-04-07 13:31:16 +0100

    all available validation rules

--- api/models/User.js
+++ api/models/User.js
@@ -16,13 +16,26 @@ module.exports = {
     firstName: {
       type: 'string',
       columnName: 'first_name',
-      required: true
+      required: true,
+      minLength: 2
     },
 
     lastName: {
       type: 'string',
       columnName: 'last_name',
       allowNull: true
+    },
+
+    username: {
+      type: 'string',
+      isNotIn: ['admin', 'super-admin', 'user'],
+      maxLength: 10
+    },
+
+    twitterURL: {
+      type: 'string',
+      columnName: 'twitter_url',
+      isURL: true
     },
 
     email: {
@@ -35,7 +48,9 @@ module.exports = {
     age: {
       type: 'number',
       isInteger: true,
-      defaultsTo: 18
+      defaultsTo: 18,
+      min: 16,
+      max: 120
     },
 
     role: {
@@ -46,15 +61,51 @@ module.exports = {
 
     petName: {
       type: 'ref',
-      isString: true
+      isString: true,
+      isNotEmptyString: true
     },
 
     favFrameworks: {
       type: 'json',
       isNumber: true
+    },
+
+    ip: {
+      type: 'string',
+      isIp: true
+    },
+
+    hasAgreedToTerms: {
+      type: 'ref',
+      isBoolean: true
+    },
+
+    themeColor: {
+      type: 'string',
+      isHexColor: true
+    },
+
+    uuid: {
+      type: 'string',
+      isUUID: true
+    },
+
+    creditCard: {
+      type: 'string',
+      isCreditCard: true
+    },
+
+    workEmail: {
+      type: 'string',
+      regex: /\W+@sailscasts\.com/L
+    },
+
+    country: {
+      type: 'string',
+      custom: function(country) {
+        return ['Ghana', 'Nigeria', 'Kenya'].includes(country)
+      }
     }
-
-
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
